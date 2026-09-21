00cc6fd60985056b8c807f7f5eb295fddf118e14
Srinagateladala <telajalasrinaga2@gmail.com>
2026-09-21 20:02:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuBtn=document.getElementById("menuBtn");const navLinks=document.getElementById("navLinks");menuBtn.addEventListener("click",()=>navLinks.classList.toggle("open"));document.querySelectorAll("#navLinks a").forEach(a=>a.addEventListener("click",()=>navLinks.classList.remove("open")));
